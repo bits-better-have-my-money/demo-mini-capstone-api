431b1e0af9cf678842afe2bc2c6329ecfbca2cb6
dani <dani.zaghian@gmail.com>
2021-09-14 10:05:48 -0700

    adds categories, join table, associations, and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,17 @@
+User.create!([
+  {name: "Ace", email: "ace@gmail.com", password: "password", password_confirmation: "password"},
+  {name: "Cheddar", email: "cheddar@gmail.com", password: "password", password_confirmation: "password"}
+])
+
 Supplier.create!([
   {name: "Thinkgeek", email: "thinkgeek@gmail.com", phone_number: "333-4444"},
   {name: "Dumbledore's Wizardry Supplies", email: "dws@gmail.com", phone_number: "555-7777"},
+])
+
+Category.create!([
+  {name: "Damage"},
+  {name: "Home Good"},
+  {name: "Outer Space"}
 ])
 
 Product.create!([
@@ -13,36 +24,22 @@ Product.create!([
 ])
 
 Image.create!([
-  {
-    url: "http://www.notcot.com/images/guide.gif",
-    product_id: 1
-  },
-  {
-    url: "http://25.media.tumblr.com/d2456964024018fd930338c099371104/tumblr_n2m73lTx2Q1svn82uo1_400.gif",
-    product_id: 2
-  },
-  {
-    url: "https://upload.wikimedia.org/wikipedia/commons/1/14/Lightsaber%2C_silver_hilt%2C_blue_blade.png",
-    product_id: 2
-  },
-  {
-    url: "http://cdn.shopify.com/s/files/1/0289/1534/products/MalPistol_MP-1_1756x988_e53f9448-81ec-41de-9369-4cbad64f18f5_1024x1024.jpg?v=1401915776",
-    product_id: 3
-  },
-  {
-    url: "https://s-media-cache-ak0.pinimg.com/736x/9c/15/7b/9c157bea5331463f9c539cbce739a4b8.jpg",
-    product_id: 4
-  },
-  {
-    url: "https://m.media-amazon.com/images/I/81KYQq0Rg6L._AC_SL1500_.jpg",
-    product_id: 4
-  },
-  {
-    url: "https://staticdelivery.nexusmods.com/mods/1151/images/12353-0-1461721930.png",
-    product_id: 5
-  },
-  {
-    url: "https://i.pinimg.com/236x/14/4a/0b/144a0bffe37f29f414e2dae2bd0141cf.jpg",
-    product_id: 6
-  },
+  {url: "http://www.notcot.com/images/guide.gif", product_id: 1},
+  {url: "http://25.media.tumblr.com/d2456964024018fd930338c099371104/tumblr_n2m73lTx2Q1svn82uo1_400.gif", product_id: 2},
+  {url: "https://upload.wikimedia.org/wikipedia/commons/1/14/Lightsaber%2C_silver_hilt%2C_blue_blade.png", product_id: 2},
+  {url: "http://cdn.shopify.com/s/files/1/0289/1534/products/MalPistol_MP-1_1756x988_e53f9448-81ec-41de-9369-4cbad64f18f5_1024x1024.jpg?v=1401915776", product_id: 3},
+  {url: "https://s-media-cache-ak0.pinimg.com/736x/9c/15/7b/9c157bea5331463f9c539cbce739a4b8.jpg", product_id: 4},
+  {url: "https://m.media-amazon.com/images/I/81KYQq0Rg6L._AC_SL1500_.jpg", product_id: 4},
+  {url: "https://staticdelivery.nexusmods.com/mods/1151/images/12353-0-1461721930.png", product_id: 5},
+  {url: "https://i.pinimg.com/236x/14/4a/0b/144a0bffe37f29f414e2dae2bd0141cf.jpg", product_id: 6},
+])
+
+ProductCategory.create!([
+  {product_id: 2, category_id: 1},
+  {product_id: 3, category_id: 1},
+  {product_id: 4, category_id: 1},
+  {product_id: 1, category_id: 3},
+  {product_id: 2, category_id: 3},
+  {product_id: 3, category_id: 3},
+  {product_id: 6, category_id: 2}
 ])
